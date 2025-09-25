55cd17bc97d5af152053304540c7e137f693b88c
Stephen Kelly <gilgalad195@gmail.com>
2025-09-25 13:58:16 -0500

    started structure for rss web connection

---
+++ internal/webconn/client.go
@@ -0,0 +1,10 @@
+package webconn
+
+import (
+	"net/http"
+	"time"
+)
+
+var client = &http.Client{
+	Timeout: time.Second * 10,
+}
---
+++ internal/webconn/request.go
@@ -0,0 +1,11 @@
+package webconn
+
+import (
+	"context"
+
+	"github.com/Gilgalad195/gatorcli/internal/rss"
+)
+
+func FetchFeed(ctx *context.Context, feedURL string) (*rss.RSSFeed, error) {
+	return nil, nil
+}
