a985b00f98bcffb50251a5b77db223c274611116
Stephen Kelly <gilgalad195@gmail.com>
2025-09-26 19:51:22 -0500

    finished FetchFeed function

---
+++ internal/webconn/httprequest.go
@@ -0,0 +1,40 @@
+package webconn
+
+import (
+	"context"
+	"encoding/xml"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/Gilgalad195/gatorcli/internal/rss"
+)
+
+var client = &http.Client{
+	Timeout: time.Second * 10,
+}
+
+func FetchFeed(ctx *context.Context, feedURL string) (*rss.RSSFeed, error) {
+	req, err := http.NewRequestWithContext(*ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "gator")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var response rss.RSSFeed
+	if err := xml.Unmarshal(body, &response); err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
